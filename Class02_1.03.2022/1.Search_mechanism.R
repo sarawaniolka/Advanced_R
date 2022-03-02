2d5eeb6d8866d16ae3dcdef58c7e1188983c1582
sarawaniolka <waniolka.sara@gmail.com>
2022-03-02 17:19:49 +0100

    comments added

--- Class02_1.03.2022/1.Search_mechanism.R
+++ Class02_1.03.2022/1.Search_mechanism.R
@@ -4,6 +4,8 @@ ls(envir = baseenv())
 ls(all.names = TRUE)
 ls(envir = baseenv(), all.names = TRUE)
 
+
+#dots are hidden, but not private
 .x <- TRUE
 ls()
 ls(all.names = TRUE)
@@ -19,13 +21,16 @@ plot <- function(x, f = identity, ...){
 }
 
 plot(seq(-pi, pi, length.out = 100), sin, col = "blue", pch = 20)
+#an infinite recurssion if we do not use "base::"
 
 ### MR: TODO
 
 ### Time series method
-x <- ts(cumsum(rnorm(100)), start = c(2000, 1), frequency = 12)x
+x <- ts(cumsum(rnorm(100)), start = c(2000, 1), frequency = 12)
 x
 class(x)
+#not plot(x)
+base::plot(x)
 plot.ts(x) # searching
 
 ### Method for data frames
@@ -33,10 +38,11 @@ y <- data.frame(x = sort(rnorm(100)))
 y <- cbind(y, data.frame(y = 1 + 2 * y$x + rnorm(100)))
 head(y)
 class(y)
-plot(y) # local definition
-graphics::plot.data.frame(y) # symbol not exported
+plot(y) # local definition -> looks bad
+graphics::plot.data.frame(y) # symbol not exported - ERROR
 graphics:::plot.data.frame(y) # calling from a namespace directly
-listEnvs1(list(environment(graphics:::plot.data.frame)))
+listEnvs1(list(environment(graphics:::plot.data.frame))) #not working, written in a last class
+
 
 rm(plot) # removing local definition
 plot(y) # search
